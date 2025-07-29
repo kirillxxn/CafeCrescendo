61beaeacb34302b2223fceeed5f4d31223efec0d
Ilya <the@kirillxxn.ru>
2025-07-30 01:35:07 +0300

    edit_files

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
 import './App.css'
 import './assets/fonts/font.css'
-import HeaderAndHome from './components/Head&Home/Head&Home'
+/* import HeaderAndHome from './components/Head&Home/Head&Home' */
 import Menu from './components/Menu/Menu'
 import { Parallax } from 'react-scroll-parallax'
 import OurCafe from './components/OurCafe/OurCafe'
+import Header from './components/Head&Home/Header/Header'
+import HomePage from './components/Head&Home/HomePage/HomePage'
 
 function App() {
 	return (
 		<>
-			<Parallax
-				translateX={['0px', '0px']}
-				scale={[0.9168, 1.287]}
-				rotate={[0, 0]}
-				easing='easeInQuad'
-			>
-				<HeaderAndHome />
+			<Header />
+			<Parallax scale={[0.961, 0.5]} opacity={[1, 0]} easing='easeInBack'>
+				<HomePage />
 			</Parallax>
 
-			<Parallax speed={-30} shouldAlwaysCompleteAnimation={false}>
+			<Parallax speed={-14} easing='easeIn'>
 				<Menu />
 			</Parallax>
 			<Parallax
 				translateX={['0px', '0px']}
-				scale={[0.1, 1]}
+				scale={[0.9, 1]}
 				rotate={[0, 0]}
-				easing='easeInQuad'
+				easing='easeIn'
+				speed={10}
 			>
 				<OurCafe />
 			</Parallax>
